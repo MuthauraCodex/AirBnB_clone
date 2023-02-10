a607d105450b34b59a43b2c74f517272dfca7580
Sarah Adekola <tadekolasarah@yahoo.com>
2023-02-09 16:46:50 -0800

    AirBnB_clone

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Unittest module for the City Class."""
+"""Unittest module for the Place Class."""
 
 import unittest
 from datetime import datetime
 import time
-from models.city import City
+from models.place import Place
 import re
 import json
 from models.engine.file_storage import FileStorage
@@ -13,9 +13,9 @@ from models import storage
 from models.base_model import BaseModel
 
 
-class TestCity(unittest.TestCase):
+class TestPlace(unittest.TestCase):
 
-    """Test Cases for the City class."""
+    """Test Cases for the Place class."""
 
     def setUp(self):
         """Sets up test methods."""
@@ -33,17 +33,17 @@ class TestCity(unittest.TestCase):
             os.remove(FileStorage._FileStorage__file_path)
 
     def test_8_instantiation(self):
-        """Tests instantiation of City class."""
+        """Tests instantiation of Place class."""
 
-        b = City()
-        self.assertEqual(str(type(b)), "<class 'models.city.City'>")
-        self.assertIsInstance(b, City)
+        b = Place()
+        self.assertEqual(str(type(b)), "<class 'models.place.Place'>")
+        self.assertIsInstance(b, Place)
         self.assertTrue(issubclass(type(b), BaseModel))
 
     def test_8_attributes(self):
-        """Tests the attributes of City class."""
-        attributes = storage.attributes()["City"]
-        o = City()
+        """Tests the attributes of Place class."""
+        attributes = storage.attributes()["Place"]
+        o = Place()
         for k, v in attributes.items():
             self.assertTrue(hasattr(o, k))
             self.assertEqual(type(getattr(o, k, None)), v)
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Unittest module for the City Class."""
+"""Unittest module for the Review Class."""
 
 import unittest
 from datetime import datetime
 import time
-from models.city import City
+from models.review import Review
 import re
 import json
 from models.engine.file_storage import FileStorage
@@ -13,9 +13,9 @@ from models import storage
 from models.base_model import BaseModel
 
 
-class TestCity(unittest.TestCase):
+class TestReview(unittest.TestCase):
 
-    """Test Cases for the City class."""
+    """Test Cases for the Review class."""
 
     def setUp(self):
         """Sets up test methods."""
@@ -33,17 +33,17 @@ class TestCity(unittest.TestCase):
             os.remove(FileStorage._FileStorage__file_path)
 
     def test_8_instantiation(self):
-        """Tests instantiation of City class."""
+        """Tests instantiation of Review class."""
 
-        b = City()
-        self.assertEqual(str(type(b)), "<class 'models.city.City'>")
-        self.assertIsInstance(b, City)
+        b = Review()
+        self.assertEqual(str(type(b)), "<class 'models.review.Review'>")
+        self.assertIsInstance(b, Review)
         self.assertTrue(issubclass(type(b), BaseModel))
 
     def test_8_attributes(self):
-        """Tests the attributes of City class."""
-        attributes = storage.attributes()["City"]
-        o = City()
+        """Tests the attributes of Review class."""
+        attributes = storage.attributes()["Review"]
+        o = Review()
         for k, v in attributes.items():
             self.assertTrue(hasattr(o, k))
             self.assertEqual(type(getattr(o, k, None)), v)
